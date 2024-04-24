56df2fc8f92c1c379c7f768f437aca627599bc68
matthewpkearney <mkearney703@gmail.com>
2024-04-23 21:42:10 -0400

    bug resolved for checking hashes (p1)

--- main.py
+++ main.py
@@ -34,17 +34,23 @@ for i in shas:
     sha_hash.append(i[-1])
 
 
+w = open('finds.txt', 'w')
+
+matches =0
 
 it = 0
-matches =0
-for i, j, k in zip(md5_hash, sha_hash, hashes):
-    # print(i, j, k)
-    print(i, k)
-    print(j, k)
-    if (i == k):
-        print(f"md5 hash match at:\n\ti={it}for:\n\thash match={hashes[it]}\n\tpassword={passwords[it]}")
-        matches=matches+1
-    if (j == k):
-        print(f"sha256 hash match at:\n\ti={it}for:\n\thash match={hashes[it]}\n\tpassword={passwords[it]}")
-        matches =matches +1
-print(matches)
+for k in hashes:
+    it2 = 0
+    for i, j in zip(md5_hash, sha_hash):
+        if (i == k):
+            print(f"md5 hash match at:\n\ti={it}for:\n\thash match={hashes[it]}\n\tpassword={passwords[it2]}\n")
+            matches=matches+1
+            w.write(f"user{it}: {passwords[it2]}")
+        if (j == k):
+            print(f"sha256 hash match at:\n\ti={it}for:\n\thash match={hashes[it]}\n\tpassword={passwords[it2]}\n")
+            matches =matches +1
+            w.write(f"user{it}: {passwords[it2]}\n")
+        it2 = it2 + 1
+    it = it +1
+
+print(f"# matches: {matches}")
